2419a8221dbcc60f03e316d04cf2d898bb295c57
Peter Heck <petersheck@gmail.com>
2019-01-22 11:42:29 -0700

    Add more code clean up

--- DictionaryType.py
+++ DictionaryType.py
@@ -1,10 +1,18 @@
 # Dictionary Type
+
+#D1
 D1 = { 'name': 'Bob', 'age': 40}
 print(D1)
+
+#D2
 D2 = { 'CTO' : D1}
 print(D2)
+
+#D3
 D3 = dict(name = 'Bob', age = 40)
 print(D3)
+
+#D4
 D4 = dict([('name', 'Bob'), ('age', 40)])
 print(D4)
 print(D4['name'])
@@ -15,6 +23,7 @@ print(D4.items())
 print(D4.get('name'))
 D4['money'] = 5000
 print(D4)
+
 for key in D4.keys():
     print(key + '\t' + str(D4[key]))
 
